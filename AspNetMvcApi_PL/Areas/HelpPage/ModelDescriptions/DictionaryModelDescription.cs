d2b4ed32eeb96d8cfdf69abc9b092e6bb8f9d152
keremgokgoz <keremgokgoz@gmail.com>
2022-05-17 11:39:42 +0300

    Add project files.

---
+++ AspNetMvcApi_PL/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AspNetMvcApi_PL.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AspNetMvcApi_PL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvcApi_PL.WebApiApplication" Language="C#" %>
