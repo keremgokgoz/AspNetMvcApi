afd8308698cdadf539a85d6f4a985d7a28a6b444
keremgokgoz <keremgokgoz@gmail.com>
2022-05-18 12:50:14 +0300

    HttpPut  ve HttpDelete metodlaru studentapiye eklendi. Postman ile test edildi.

--- AspNetMvcApi_PL/Controllers/StudentController.cs
+++ AspNetMvcApi_PL/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using AspNetMvcApi_EL.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Messaging;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -48,7 +49,7 @@ namespace AspNetMVCApi_PL.Controllers
         [HttpPost]
         [System.Web.Http.Route("")]
 
-        public ResponseData AddStudent([FromBody]StudentVM model)
+        public ResponseData AddStudent([FromBody] StudentVM model)
         {
             try
             {
@@ -84,5 +85,92 @@ namespace AspNetMVCApi_PL.Controllers
         // PUT api/<controller>/5
 
         // DELETE api/<controller>/5
+        [HttpPut]
+        [System.Web.Http.Route("")]
+        public ResponseData UpdateStudent(int studentId,string name,string surname) {
+            try
+            {
+                if (studentId > 0)
+                {
+                  ResponseData result=  _studentService.UpdateStudent(studentId, name, surname);
+                    if (result.IsSuccess)
+                    {
+                        return new ResponseData()
+                        {
+                            IsSuccess = true,
+                            Message = "Öğrenci başarılı bir şekilde güncellenmiştir."
+                        };
+                    }
+                    else
+                    {
+                        return new ResponseData()
+                        {
+                            IsSuccess = false,
+                            Message = "HATA. Öğrenci güncelleme işleminde bir sorun oluştu."
+                        };
+                    }
+                }
+                else
+                {
+                    return new ResponseData()
+                    {
+                        IsSuccess = false,
+                        Message = "Öğrenci Bulunamadı"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new ResponseData()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        
+        }
+
+        [HttpDelete]
+        [System.Web.Http.Route("")]
+        public ResponseData DeleteStudent(int id)
+        {
+            try
+            {
+                if (id>0)
+                {
+                    var result = _studentService.DeleteStudent(id);
+                    if (result.IsSuccess)
+                    {
+                        return new ResponseData()
+                        {
+                            IsSuccess = true,
+                            Message="Öğrenci sistemden silinmiştir."
+                        };
+                    }
+                    else
+                    {
+                        return new ResponseData()
+                        {
+                            IsSuccess = false,
+                            Message = "HATA. Öğrenci sistemden silinemedi !"
+                        };
+                    }
+                }
+                else
+                {
+                    return new ResponseData()
+                    {
+                        IsSuccess = false,
+                        Message="HATA. id girişi bilgisi düzgün olmalıdır."
+                    };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
